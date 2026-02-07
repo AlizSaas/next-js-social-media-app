44e5dfdba2ebeedcb1ff279337444850b437c853
zxzAli <blackbearddevss@gmail.com>
2026-02-07 13:58:11 -0500

    fix: allow UploadThing image domains in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -6,15 +6,21 @@ const nextConfig = {
     }, // This sets the stale time for dynamic pages to 30 seconds 
   },
   serverExternalPackages: ["@node-rs/argon2"], // This allows the use of the @node-rs/argon2 package on the server side 
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "utfs.io",
-        pathname: `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/*`,
-      },
-    ],
-  }, // This allows images from the UploadThing service to be used in the app
+ images: {
+  remotePatterns: [
+    {
+      protocol: "https",
+      hostname: "utfs.io",
+      pathname: "/**",
+    },
+    {
+      protocol: "https",
+      hostname: "*.ufs.sh",
+      pathname: "/**",
+    },
+  ],
+},
+ // This allows images from the UploadThing service to be used in the app
   rewrites: () => {
     return [
       {
